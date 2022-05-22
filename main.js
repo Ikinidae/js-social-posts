a008b30aa3e3e3b38914c662752a2f311fc5dce4
Ikinidae <federicaelia24@gmail.com>
2022-05-22 14:33:53 +0200

    Quasi finita prima milestone

--- main.js
+++ main.js
@@ -55,3 +55,86 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+// comandi per header post
+const container = document.getElementById("container");
+
+const postDiv = document.createElement("div")
+postDiv.classList.add("post");
+container.append(postDiv);
+
+const postHeader = document.createElement("div");
+postHeader.classList.add("post__header");
+postDiv.append(postHeader);
+
+const postMeta = document.createElement("div");
+postMeta.classList.add("post-meta");
+postHeader.append(postMeta);
+const postMetaIcon = document.createElement("div");
+postMetaIcon.classList.add("post-meta__icon");
+postMeta.append(postMetaIcon);
+const authorImg = document.createElement("img");
+authorImg.src = posts[0].author.image;
+authorImg.classList.add("profile-pic");
+postMetaIcon.append(authorImg);
+
+const postMetaData = document.createElement("div");
+postMetaData.classList.add("post-meta__data");
+postMeta.append(postMetaData);
+const postMetaAuthor = document.createElement("div");
+postMetaAuthor.classList.add("post-meta__author");
+const authorText = document.createTextNode(posts[0].author.name);
+postMetaAuthor.append(authorText);
+
+postMetaData.append(postMetaAuthor);
+const postMetaTime = document.createElement("div");
+postMetaTime.classList.add("post-meta__time");
+const postMetaTimeText = document.createTextNode(posts[0].created);
+postMetaData.append(postMetaTime);
+postMetaTime.append(postMetaTimeText);
+
+// comandi corpo post
+const postText = document.createElement("div");
+postText.classList.add("post__text");
+const postText1 = document.createTextNode(posts[0].content);
+postText.append(postText1);
+postDiv.append(postText);
+
+const postImg = document.createElement("div");
+postImg.classList.add("post__image");
+postDiv.append(postImg);
+const postImgImg = document.createElement("img");
+postImgImg.src = posts[0].media;
+postImg.append(postImgImg);
+
+// comandi footer post
+const postFooter = document.createElement("div");
+postFooter.classList.add("post__footer");
+postDiv.append(postFooter);
+
+const likes = document.createElement("div");
+likes.classList.add("likes", "js-likes");
+postFooter.append(likes);
+const likesCta = document.createElement("div");
+likesCta.classList.add("likes__cta");
+likes.append(likesCta);
+
+const likeButton = document.createElement("a");
+likeButton.classList.add("like-button", "js-like-button");
+likeButton.href = "#";
+likesCta.append(likeButton);
+const likeButtonIcon = document.createElement("i");
+likeButtonIcon.classList.add("like-button__icon", "fas", "fa-thumbs-up");
+likeButtonIcon.ariaHidden = "true";
+likeButton.append(likeButtonIcon);
+const likeButtonLabel = document.createElement("span");
+likeButtonLabel.classList.add("like-button__label");
+const likeButtonSpan = document.createTextNode(" Mi Piace");
+likeButtonLabel.append(likeButtonSpan);
+likeButton.append(likeButtonLabel);
+
+const likesCounter = document.createElement("div");
+likesCounter.classList.add("likes__counter");
+const likesCounterText = document.createTextNode("Piace a 81 persone");
+likesCounter.append(likesCounterText);
+postFooter.append(likesCounter);
